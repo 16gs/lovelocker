1d930dacbdb9c324a0b5492c20d965f0f31be749
marshmellow <arabianseed27@gmail.com>
2025-11-02 18:19:03 +0300

    Update vault.js

--- vault.js
+++ vault.js
@@ -8,7 +8,30 @@ function saveMemory() {
     savedMemoryDisplay.textContent = 'Please enter a memory to save.';
     console.log("Memory saved!");
   }
+ document.getElementById("uploadForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  const fileInput = document.getElementById("selfieInput");
+  const status = document.getElementById("uploadStatus");
+
+  if (fileInput.files.length === 0) {
+    status.textContent = "Please select a selfie to upload 💭";
+    status.style.color = "red";
+    return;
+  }
+
+  const file = fileInput.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function() {
+    localStorage.setItem("uploadedSelfie", reader.result);
+    status.textContent = "Selfie uploaded securely 💘";
+    status.style.color = "hotpink";
+  };
+
+  reader.readAsDataURL(file);
+});
 }
+
 
 
 
