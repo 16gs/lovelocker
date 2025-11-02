121e06b4795632bfe1b1dd0ae384139839cfae23
marshmellow <arabianseed27@gmail.com>
2025-11-02 18:32:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function checkPassword() {
   const input = document.getElementById("password").value;
   const error = document.getElementById("error");
 
-  if (input === "lovelocker") {
+  if (input === "zamzam") {
     window.location.href = "vault.html";
   } else {
     error.textContent = "Incorrect password. Try again 💔";
     error.style.color = "red";
   }
 }
+
 
 
 
